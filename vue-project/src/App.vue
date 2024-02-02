1f9d6d1201c936a270a0a225162c5429fe1c841a
shiba mitsue <mitue.okamoto@gmail.com>
2024-02-02 18:52:23 +0900

    FirstStep

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-//復習
-const right = 3;
-const left = 4;
-const result = right * left;
-
-// 初級: 
-// * 右辺と左辺にランダムな1桁の数字を表示する
-//// ヒント: Math.floor(Math.random()*10) を使う
-
-// * 左辺と右辺を掛けて答えを表示する
+import FirstStep from '@/components/FirstStep.vue';
 
 </script>
 
 <template>
   <main class="main">
-    {{ right }} × {{ left }} = {{ result }}
+    <FirstStep />
   </main>
 </template>
 
---
+++ vue-project/src/components/FirstStep.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+//復習
+const right = 3;
+const left = 4;
+const result = right * left;
+
+// 初級: 
+// * 右辺と左辺にランダムな1桁の数字を表示する
+//// ヒント: Math.floor(Math.random()*10) を使う
+
+// * 左辺と右辺を掛けて答えを表示する
+
+</script>
+
+
+<template>
+  <main class="main">
+    {{ right }} × {{ left }} = {{ result }}
+  </main>
+</template>
