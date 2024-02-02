8a976474560425e8bea18aa6326cde969b780d2a
shiba mitsue <mitue.okamoto@gmail.com>
2024-02-02 18:50:11 +0900

    firstcommit 初級編

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+//復習
+const right = 3;
+const left = 4;
+const result = right * left;
+
+// 初級: 
+// * 右辺と左辺にランダムな1桁の数字を表示する
+//// ヒント: Math.floor(Math.random()*10) を使う
+
+// * 左辺と右辺を掛けて答えを表示する
+
+</script>
+
+<template>
+  <main class="main">
+    {{ right }} × {{ left }} = {{ result }}
+  </main>
+</template>
+
+<style scoped>
+/* ここでスタイルを設定します。 */
+.main {
+  font-size: 16vw;
+  text-align: center;
+}
+</style>
